43a7ba69b57f59f57d166dd2f7cd99557eebe302
angela_becerra <angelabecerraforero@gmail.com>
2022-05-03 18:33:28 -0500

    Se instala Firebase al proyecto, se hacen modificaciones en archivos como app.js, index.js y se crea archivo firebase.js para traer todo el codigo de firebase y se instala react

---
+++ lab-notes/src/firebase.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import 'firebase/firestore'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAU-x84UFLb858FBu5F6BW29fR_oYt3PJc",
+  authDomain: "lab-notes-27a9a.firebaseapp.com",
+  projectId: "lab-notes-27a9a",
+  storageBucket: "lab-notes-27a9a.appspot.com",
+  messagingSenderId: "621669163186",
+  appId: "1:621669163186:web:004f840881331b736a69ab",
+  measurementId: "G-SZQ6N0PFQR"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const db = app.firestore();
