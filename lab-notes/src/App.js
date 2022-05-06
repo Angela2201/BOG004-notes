10c57fa49982f7c768f5c931dc7f0cbbc7560e12
angela <angelabecerraforero@gmail.com>
2022-05-06 16:53:01 -0500

    Se empieza a crear codigo para autenticar con Gmail por medio de Firebase

--- lab-notes/src/App.js
+++ lab-notes/src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
 /* import './App.css'; */
 import {
+  Link,
   Route,
   Routes
 } from "react-router-dom";
+import Home from './components/Home'
 
 //Se crean componentes para hacre el ruteo
-const Home = () => <h1>Home</h1>
+//const Home = () => <h1>Home</h1>
 const Wall = () => <h1>Wall</h1>
 
 function App() {
   return (
     <div className='container'>
-      <h1>HELLO WORLD</h1>
+      <header>
+        <nav>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/wall'>Wall</Link></li>
+          </ul>
+        </nav>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/wall' element={<Wall />} />
         </Routes>
+      </header>
     </div>
   );
 }
---
+++ lab-notes/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div id="home">
+            <h1>Developer Lab Notes</h1>
+            <p>Aqui tu primer nota</p>
+            <button>
+            <img className="logoGoogle" src="https://i.imgur.com/bD3SqPX.png" alt="logoGoogle"></img>
+            </button>       
+        </div>
+    )
+}
+
+export default Home
