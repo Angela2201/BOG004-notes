05f490d1e25256878ef85e3570a86fb75e132522
angela_becerra <angelabecerraforero@gmail.com>
2022-05-05 19:01:57 -0500

    Se logra realizar parte del ruteo para home y para wall

--- lab-notes/src/App.js
+++ lab-notes/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import './App.css';
+/* import './App.css'; */
+import {
+  Route,
+  Routes
+} from "react-router-dom";
+
+//Se crean componentes para hacre el ruteo
+const Home = () => <h1>Home</h1>
+const Wall = () => <h1>Wall</h1>
 
 function App() {
   return (
-    <h1>HELLO WORLD</h1>
+    <div className='container'>
+      <h1>HELLO WORLD</h1>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/wall' element={<Wall />} />
+        </Routes>
+    </div>
   );
 }
 
